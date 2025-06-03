b0e1abcefb4794d42f62d675d780bc5e0e09038e
Lorenzo Bonin <lollozanetti04@gmail.com>
2025-06-03 03:01:32 +0200

    substituting cluster for dependencies issues

--- modules/qcnet_decoder.py
+++ modules/qcnet_decoder.py
@@ -17,8 +17,8 @@ from typing import Dict, List, Mapping, Optional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_cluster import radius
-from torch_cluster import radius_graph
+from utils import radius
+from utils import radius_graph
 from torch_geometric.data import Batch
 from torch_geometric.data import HeteroData
 from torch_geometric.utils import dense_to_sparse
