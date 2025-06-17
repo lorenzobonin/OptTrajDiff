a4881a012fb218170985e40d9124c57b38f1c6ae
Lorenzo Bonin <lbonin00@login01.leonardo.local>
2025-06-18 01:39:09 +0200

    removed torch scatter and cluster

--- losses/mixture_of_laplace_nll_loss.py
+++ losses/mixture_of_laplace_nll_loss.py
@@ -16,7 +16,7 @@ from typing import Optional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_scatter import segment_csr
+from utils import segment_csr
 
 from losses.laplace_nll_loss import LaplaceNLLLoss
 
